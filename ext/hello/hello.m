f3184c9ad443e5b58f27abb884c33000d9e2a8ec
youpy <youpy@buycheapviagraonlinenow.com>
2015-04-16 21:41:07 +0900

    add RbInternal

--- ext/hello/hello.m
+++ ext/hello/hello.m
@@ -1,16 +1,6 @@
 #include "hello.h"
 
-VALUE rb_mHello;
-VALUE rb_cWorld;
-
-VALUE cWorld_greeting(int argc, VALUE *argv, VALUE self)
-{
-  return [[[Hello alloc] init] greeting];
-}
-
 void Init_hello(void)
 {
-  rb_mHello = rb_define_module("Hello");
-  rb_cWorld = rb_define_class_under(rb_mHello, "World", rb_cObject);
-  rb_define_method(rb_cWorld, "greeting", cWorld_greeting, -1);
+  [RbInternal initRb];
 }
--- ext/hello/hello_class.swift
+++ ext/hello/hello_class.swift
@@ -1,4 +1,33 @@
-@objc class Hello: NSObject {
+typealias RbMethod = @objc_block (Int32, UnsafeMutablePointer<VALUE>, VALUE) -> VALUE
+typealias RbMethodPointer = CFunctionPointer<(() -> VALUE)>
+
+class RbInternal: NSObject {
+    static func initRb() {
+        var rb_mHello = rb_define_module("Hello")
+        var rb_cWorld = rb_define_class_under(rb_mHello, "World", rb_cObject)
+
+        self.defineMethodWithVariableArguments(
+            rb_cWorld, name:"greeting", block:{
+                (argc, argv, _self) in
+                return Hello().greeting()
+            }
+        )
+    }
+
+    static func defineMethodWithVariableArguments(klass: VALUE, name: String, block: RbMethod) {
+        rb_define_method(klass, name, self.convertBlock2RbMethodPointer(block), -1)
+    }
+
+    // http://stackoverflow.com/a/29375116
+    static func convertBlock2RbMethodPointer(block: RbMethod) -> RbMethodPointer {
+        var imp: COpaquePointer = imp_implementationWithBlock(unsafeBitCast(block, AnyObject.self))
+        var callback: RbMethodPointer = unsafeBitCast(imp, RbMethodPointer.self)
+
+        return callback
+    }
+}
+
+class Hello: NSObject {
     func greeting() -> VALUE {
         return rb_str_new_cstr("hello from Swift")
     }
