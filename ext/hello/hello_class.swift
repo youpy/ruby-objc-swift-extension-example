1dcd4e01e647e671bc6de21e4fc042f0fe23b25d
youpy <youpy@buycheapviagraonlinenow.com>
2015-04-20 20:37:28 +0900

    improve variable declation

--- ext/hello/hello_class.swift
+++ ext/hello/hello_class.swift
@@ -6,11 +6,11 @@ typealias RbMethod2 = @objc_block (VALUE, VALUE, VALUE) -> VALUE
 
 class RbInternal: NSObject {
     static func initRb() {
-        var rb_mHello = rb_define_module("Hello")
-        var rb_cSwiftObject = rb_define_class("SwiftObject", rb_cObject)
-        var rb_cWorld = rb_define_class_under(rb_mHello, "World", rb_cSwiftObject)
+        let rb_mHello = rb_define_module("Hello")
+        let rb_cSwiftObject = rb_define_class("SwiftObject", rb_cObject)
+        let rb_cWorld = rb_define_class_under(rb_mHello, "World", rb_cSwiftObject)
 
-        var block: RbMethod1 = {
+        let block: RbMethod1 = {
             (_self, name) in
             return self.string2RbString(
                        Hello().greeting(self.rbString2String(name)!)
@@ -20,7 +20,7 @@ class RbInternal: NSObject {
     }
 
     static func defineMethod<T>(klass: VALUE, name: String, block: T) {
-        var argc: Int32
+        let argc: Int32
 
         switch block {
         case is RbMethod0:
@@ -38,8 +38,8 @@ class RbInternal: NSObject {
 
     // http://stackoverflow.com/a/29375116
     static func convertBlock2RbMethodPointer<T>(block: T) -> RbMethodPointer {
-        var imp: COpaquePointer = imp_implementationWithBlock(unsafeBitCast(block, AnyObject.self))
-        var callback: RbMethodPointer = unsafeBitCast(imp, RbMethodPointer.self)
+        let imp: COpaquePointer = imp_implementationWithBlock(unsafeBitCast(block, AnyObject.self))
+        let callback: RbMethodPointer = unsafeBitCast(imp, RbMethodPointer.self)
 
         return callback
     }
